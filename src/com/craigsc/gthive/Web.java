f3826fe2fa5d54d50b5093feb680646983b3c00c
Craig Campbell <craigsc333@gmail.com>
2010-04-04 18:45:14 -0400

    Added res/ and src/ directories.

---
+++ src/com/craigsc/gthive/Web.java
@@ -0,0 +1,40 @@
+package com.craigsc.gthive;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Window;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Web extends Activity {
+	WebView browser;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.web);
+		browser = (WebView) findViewById(R.id.webview);
+		browser.setWebViewClient(new CustomWebViewClient());
+		browser.getSettings().setJavaScriptEnabled(true);
+		browser.loadUrl("http://www.mail.gatech.edu");
+	}
+	
+	private class CustomWebViewClient extends WebViewClient {
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			view.loadUrl(url);
+			return true;
+		}
+	}
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if ((keyCode == KeyEvent.KEYCODE_BACK) && browser.canGoBack()) {
+			browser.goBack();
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+}
