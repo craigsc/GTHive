7bcb5fedbde0e8cdeffc3ea01fa11a3b08c9203f
Craig Campbell <craigsc333@gmail.com>
2010-09-25 02:21:09 -0400

    Repaired the auto-signin feature for all 2.2 devices. The broadcast receiver now uses a Service to perform the actual authentication to prevent ANR errors.

--- src/com/craigsc/gthive/GTHiveReceiver.java
+++ src/com/craigsc/gthive/GTHiveReceiver.java
@@ -12,6 +12,7 @@ import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
+import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -19,6 +20,7 @@ import android.content.SharedPreferences;
 import android.net.NetworkInfo;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -30,50 +32,64 @@ public class GTHiveReceiver extends BroadcastReceiver {
 			NetworkInfo ni = (NetworkInfo) intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
 			WifiInfo wi = ((WifiManager) context.getSystemService(Context.WIFI_SERVICE)).getConnectionInfo();
 			if (ni.isConnected() && wi != null && wi.getSSID().equalsIgnoreCase(GTHive.SSID)) {
-				SharedPreferences settings = context.getSharedPreferences(GTHive.PREFS_NAME, 0);
-				String user = settings.getString("user", null);
-				String pass = settings.getString("pass", null);
-				if (user != null && pass != null) {
-					
-					HttpClient client = new DefaultHttpClient();
-					HttpPost post = new HttpPost("https://auth.lawn.gatech.edu/index.php");
-					try {
-						//set up post parameters
-						List<NameValuePair> data = new ArrayList<NameValuePair>(3);
-						data.add(new BasicNameValuePair("username", user));
-						data.add(new BasicNameValuePair("password", pass));
-						data.add(new BasicNameValuePair("output", "text"));
-						//check preferences for ISS status
-						if (Prefs.getISS(context)) {
-							Log.d("GTHive", "ISS ENABLED");
-							data.add(new BasicNameValuePair("iss", "on"));
-						}
-						post.setEntity(new UrlEncodedFormEntity(data));
-						
-						ResponseHandler<String> rh = new BasicResponseHandler();
-						String result = client.execute(post,rh);
-						
-						//check text response and alert user of success/failure
-						if (result.equals("Logging you into LAWN...")) {
-							result = "Successfully logged into LAWN";
-						}
-						else if (result.equals("")) {
-							result = "Already logged into LAWN.";
-						} 
-						display(context, result);
-					} catch (Exception e) {
-						display(context, "Error authenticating to LAWN. Try using GTHive.");
-						Log.d("GTHive_BROADCAST_RECEIVER", e.toString());
-					}
-					
-				}
+				context.startService(new Intent(context, LoginService.class));
 			}
 		}
 	}
 	
-	private void display(Context c, String text) {
-		if (Prefs.showAutoSigninMessages(c)) {	
-			Toast.makeText(c, text, Toast.LENGTH_LONG).show();
+	public static class LoginService extends Service {
+
+		@Override
+		public void onStart(Intent intent, int startId) {
+			Context context = this;
+			
+			SharedPreferences settings = context.getSharedPreferences(GTHive.PREFS_NAME, 0);
+			String user = settings.getString("user", null);
+			String pass = settings.getString("pass", null);
+			if (user != null && pass != null) {
+					
+				HttpClient client = new DefaultHttpClient();
+				HttpPost post = new HttpPost("https://auth.lawn.gatech.edu/index.php");
+				try {
+					//set up post parameters
+					List<NameValuePair> data = new ArrayList<NameValuePair>(3);
+					data.add(new BasicNameValuePair("username", user));
+					data.add(new BasicNameValuePair("password", pass));
+					data.add(new BasicNameValuePair("output", "text"));
+					//check preferences for ISS status
+					if (Prefs.getISS(context)) {
+						Log.d("GTHive", "ISS ENABLED");
+						data.add(new BasicNameValuePair("iss", "on"));
+					}
+					post.setEntity(new UrlEncodedFormEntity(data));
+						
+					ResponseHandler<String> rh = new BasicResponseHandler();
+					String result = client.execute(post,rh);
+					//check text response and alert user of success/failure
+					if (result.equals("Logging you into LAWN...")) {
+						result = "Successfully logged into LAWN";
+					}
+					else if (result.equals("")) {
+						result = "Already logged into LAWN.";
+					} 
+					display(context, result);
+				} catch (Exception e) {
+					display(context, "Error authenticating to LAWN. Try using GTHive.");
+					Log.d("GTHive_BROADCAST_RECEIVER", e.toString());
+				}
+			}
 		}
+		
+		private void display(Context c, String text) {
+			if (Prefs.showAutoSigninMessages(c)) {	
+				Toast.makeText(c, text, Toast.LENGTH_LONG).show();
+			}
+		}
+		
+		@Override
+		public IBinder onBind(Intent intent) {
+			return null;
+		}
+		
 	}
 }
